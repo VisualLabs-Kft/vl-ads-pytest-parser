b81903f121b8a1f7e12c1b9355f77985e5717dec
Aron Pal-Jakab <aron.paljakab@visuallabs.hu>
2023-03-11 14:08:24 +0100

    Added files with the required pypi file structure

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vl-ads-pytest-parser
+# Pytest Parser to Azure DevOps Test Case
 Associate DevOps test cases with pytest XML results
+
+
+## Usage
+The parser should be run on an Azure DevOps ```pytest-azurepipelines``` generated XML file, because it searches for the following attributes:
+- testcase
+  - name
+  - result
+
+The Python script requires the following arguments:
+- org: Azure DevOps organization name
+- project: Azure DevOps project name
+- plan: Azure DevOps test plan name
+- suite: Azure DevOps test suite name
+- auth: Azure DevOps personal access token
+- xml: pytest-azurepipelines generated XML file
+
+For example:
+```
+ads-pytest-parser org project planID suiteID authToken test.xml
+```
+
+The script will then associate the test cases with the test results - for the test cases the outcome will be set.
---
+++ src/ads_pytest_parser/assoc_devops_testcase_pytest_results.py
@@ -0,0 +1,75 @@
+# Associate DevOps test cases with pytest XML results
+# The code will parse the JUnit XML file and send the results to DevOps Test Plan
+# The test cases should be named like this: testcase_1234, where 1234 is the test case ID in DevOps
+# The module should be called with the following arguments: org, project, plan, suite, auth (token)
+
+import xml.etree.ElementTree as ET
+import requests
+import json
+import argparse
+
+# TODO: add error logging into the json body
+# TODO: how to run this code in the pipeline and how to get the test.xml file in the pipeline?
+# TODO: add error handling
+# TODO: mark the test case with ID?
+# TODO: add logging for pypi
+
+# Parse the arguments
+parser = argparse.ArgumentParser(
+    description="Parse the JUnit XML file and send the results to DevOps Test Plan",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("org", help="DevOps organization")
+parser.add_argument("project", help="DevOps project")
+parser.add_argument("plan", help="DevOps Test Plan ID")
+parser.add_argument("suite", help="DevOps Test Suite ID")
+parser.add_argument("auth", help="DevOps Authetication Token")
+parser.add_argument("xml", help="Path to XML file")
+
+args = vars(parser.parse_args())
+# Commmand line arguments, DevOps variables and secrets
+ORGANIZATION = args["org"]
+PROJECT = args["project"]
+PLAN_ID = args["plan"]
+SUITE_ID = args["suite"]
+AUTH_TOKEN = args["auth"]
+XML_FILE = args["xml"]
+
+# Parse the JUnit XML file
+tree = ET.parse(XML_FILE)
+root = tree.getroot()
+
+
+# Construct the URL and the header
+URL = f"https://dev.azure.com/{ORGANIZATION}/{PROJECT}/_apis/testplan/Plans/{PLAN_ID}/Suites/{SUITE_ID}/TestPoint?api-version=7.0"
+headers = {"Authorization": f"Basic {AUTH_TOKEN}", "Content-Type": "application/json"}
+
+body = []
+
+# Loop through each test case in the XML file
+for testcase in root.iter("testcase"):
+
+    # Get the test case ID and test result
+    # Tests should be named like this: testcase_1234, where 1234 is the test case ID in DevOps
+    case_id = testcase.get("name").split("_")[1]
+    result = testcase.get("result")
+
+    # Print the results
+    # print(f"Test case ID: {case_id}, Result: {result}")
+
+    # Build json object
+    test_case_json = {
+        "id": case_id,
+        "results": {
+            "outcome": "passed" if result == "Passed" else "failed",
+        },
+    }
+
+    # create a json array
+    body.append(test_case_json)
+
+# print(body)
+
+# send an http request to the URL
+response = requests.patch(URL, headers=headers, data=json.dumps(body))
+print(response)
